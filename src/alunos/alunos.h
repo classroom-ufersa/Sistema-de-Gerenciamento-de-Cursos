4d56a1502f1c2fc28a3a9c4133bab9a96fa92714
johanpq <johanp.queiroz@gmail.com>
2023-09-19 15:35:24 -0300

    feat: protótipos adicionados

--- src/alunos/alunos.h
+++ src/alunos/alunos.h
@@ -1,3 +1,7 @@
+/*Renomeia a struct*/
 typedef struct alunos Alunos;
-
+/*Renomeia a struct*/
 typedef struct listaalunos ListaAlunos;
+
+/*Função para criar alunos*/
+Alunos *Criar_Alunos(char *nome, int numero_matricula);
--- src/cursos/cursos.h
+++ src/cursos/cursos.h
@@ -1,1 +1,6 @@
+/*Função para alocar a struct dinâmicamente*/
 typedef struct cursos Cursos;
+
+/*Função para criar os cursos*/
+Cursos *Criar_Cursos(char *nome, int codigo, int vagas);
+
