ba4a14c4014906d6c2f376571b12514f48b8122a
Servolo P. <138056513+srlv@users.noreply.github.com>
2023-10-11 18:07:40 +0000

    atualização

--- src/alunos/alunos.h
+++ src/alunos/alunos.h
@@ -1,19 +1,21 @@
-//#include "../cursos/cursos.h"
+#ifndef ALUNO_H
+#define ALUNO_H
+
+#include "../cursos/cursos.h"
+
 /* Renomeia a struct */
 typedef struct alunos Alunos;
+
 /*Renomeia a struct*/
-typedef struct listaalunos ListaAlunos;
 
 /* Função para criar alunos */
 Alunos *Criar_Alunos(char *nome, int numero_matricula, float notas[3]);
 
 void Imprimir_Alunos(Alunos *Var_Alunos, int contador);
 
-ListaAlunos *CriarAluno(Alunos *Var_Alunos);
-
-void AdicionarAluno(ListaAlunos **lista, Alunos *Var_Alunos);
-
-void ImprimirAlunos(ListaAlunos *lista);
+int CompararMatricula(Alunos *Var_Alunos, int NumeroMatricula, int contador);
 
 /* Função para a criação do menu */
 void menu();
+
+#endif //ALUNO_H
