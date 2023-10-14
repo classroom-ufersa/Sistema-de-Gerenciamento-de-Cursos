2f8496ff7afce9220a0d923f0aa94f8ade7d526d
Servolo P. <138056513+srlv@users.noreply.github.com>
2023-10-14 15:34:31 +0000

    Função para editar matricula feita

--- src/cursos/cursos.h
+++ src/cursos/cursos.h
@@ -23,6 +23,10 @@ void ExcluirMatricula(ListaMatricula **lista, char *NomeAluno, int CodigoDiscipl
 
 void BuscarCurso(Cursos *cursos, char *NomeCurso, int contador);
 
-void EditarMatriculaAluno(ListaMatricula **lista, char* nomeAlunoEditar, char* novoNomeAluno, Alunos *aluno, int contador);
+void EditarNomeAluno(ListaMatricula **lista, char* nomeAlunoEditar, char* novoNomeAluno, Alunos *aluno, int contador);
+
+void EditarMatriculaAluno(ListaMatricula **lista, int MatriculaAlunoEditar, int NovaMatriculaAluno, Alunos* aluno, int contador);
+
+void EditarNotaAluno(ListaMatricula **lista, float NotaEditar, float NovaNota, Alunos* aluno, int contador);
 
 #endif //CURSOS_H
